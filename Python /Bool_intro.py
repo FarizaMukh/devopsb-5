2a4f74514c74e8d25b68ae519d04d3a3c970acad
Fariza Mukhtarova <FreezyBreezy@Farizas-Air.fios-router.home>
2023-09-25 19:08:05 -0500

    Bool Notes Added

---
+++ Python /Bool_intro.py
@@ -0,0 +1,18 @@
+#Creating a boolean variable
+is_rainy = False 
+print(is_Rainy)
+#Let's look at the type of the variable 
+print(type(is_Rainy)) #<class 'bool'>
+
+is_September = True 
+print(is_September)
+#Let's look at the type of the variable 
+print(type(is_Rainy)) #<class 'bool'>
+
+
+
+
+
+
+
+
